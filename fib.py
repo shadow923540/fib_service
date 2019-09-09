622f1e426c4b657842200f250a146c03eb95de62
shadow923540 <Naruto#sasuke123!>
2019-09-09 16:40:10 +0200

    refactor

--- fib.py
+++ fib.py
@@ -2,23 +2,23 @@ from flask_restful import Resource
 import functools
 
 @functools.lru_cache(None)
-def fibb(n):
+def calculate_fibonacci(n):
     if n < 2:
         return n
-    return fibb(n-1) + fibb(n-2)
+    return calculate_fibonacci(n-1) + calculate_fibonacci(n-2)
 
 class FibNum(Resource):
     def get(self, fib):
         try:
             if fib <= 1:
-                return fibb
+                return fib
             else:
-                return fibb(fib - 1) + fibb(fib - 2)
+                return calculate_fibonacci(fib - 1) + calculate_fibonacci(fib - 2)
             return {'message': 'Something wrong'}, 404
         except RecursionError:
-            return 'Please put smaller value to calculate fibonacci number to avoid Recursion Error'
+            return 'Please put smaller value to calculate fibonacci number to avoid Recursion Error', 400
 
 
 class FibString(Resource):
     def get(self, name):
-        return 'Wrong value: '+ name + '.  Please create endpoint with correct int value for fibonacci calculation'
+        return 'Wrong value: '+ name + '.  Please create endpoint with correct int value for fibonacci calculation', 404
