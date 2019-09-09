1e5c1711dbb81ac721e8d1c4862e7a6e50574675
shadow923540 <Naruto#sasuke123!>
2019-09-09 16:04:00 +0200

    Add optimization

--- flask_restful_app.py
+++ flask_restful_app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from fib import FibNum
+from fib import FibNum, FibString
 
 app = Flask(__name__)
 api= Api(app)
 
 api.add_resource(FibNum, '/fib/<int:fib>')
+api.add_resource(FibString, '/fib/<string:name>')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
